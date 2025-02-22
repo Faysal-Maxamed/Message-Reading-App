323cc0e279054ca5257f36638576573e68fd1c2e
Faysal-Maxamed <pheymoha17@gmail.com>
2025-02-22 21:13:19 +0300

    ..

--- lib/SmsController.dart
+++ lib/SmsController.dart
@@ -1,9 +1,23 @@
-import 'package:flutter/material.dart';
+import 'package:another_telephony/telephony.dart';
 import 'package:get/get.dart';
-import 'package:reading_messege_app/home_screen.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-void main() {
-  runApp(GetMaterialApp(
-    home: HomeScreen(),
-  ));
+class Smscontroller extends GetxController {
+  var allMessages = <SmsMessage>[].obs;
+
+  requstsms() async {
+    var status = await Permission.sms.request();
+    if (status.isGranted) {
+      fetchInboxMessages();
+    } else {
+      print("Ogolaanshiyaha lama oggolaan!");
+    }
+  }
+
+  fetchInboxMessages() async {
+    final Telephony telephony = Telephony.instance;
+    List<SmsMessage> messages = await telephony.getInboxSms();
+    allMessages.value = messages;
+    
+  }
 }
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Messege App",
+          style: TextStyle(fontSize: 24),
+        ),
+        elevation: 10,
+        shadowColor: Colors.teal,
+        backgroundColor: Colors.teal,
+        centerTitle: true,
+        foregroundColor: Colors.white,
+      ),
+      body: Text("Hellow"),
+    );
   }
 }
